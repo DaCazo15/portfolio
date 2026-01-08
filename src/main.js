27698118ef0c89afef19202505979310b2e877c5
Daniel Cazorla <dcazorla.0190@gmail.com>
2026-01-08 12:25:38 -0400

    change-update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
+
+// Outfit
+import '@fontsource/outfit/400.css';
+import '@fontsource/outfit/700.css';
+
+// Rubik
+import '@fontsource/rubik/300.css';
+import '@fontsource/rubik/400.css';
+import '@fontsource/rubik/700.css';
+
 import App from './App.vue'
 
 createApp(App).mount('#app')
